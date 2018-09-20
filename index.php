7a68d2816c3cdaed03216bc7cc193a66785ca66c
lightwill0309 <brilliantwish54@gmail.com>
2018-09-20 08:50:55 +0000

    build.yml

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>FuturePop</title>
+<style>
+body {
+  background-color: #000;
+  color: #fb1;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+<h1>FuturePop</h1>
+</body>
+</html>
